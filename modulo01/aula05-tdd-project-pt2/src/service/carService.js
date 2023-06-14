3309fbda56bebfd6891f05d022ff81a01d01fbea
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-06-14 20:02:10 -0300

    Projeto Final - TDD & BDD - Parte 02

---
+++ modulo01/aula05-tdd-project-pt2/src/service/carService.js
@@ -0,0 +1,28 @@
+const BaseRepository = require('../repository/base/baseRepository');
+
+class CarService {
+  constructor({ cars }) {
+    this.carRepository = new BaseRepository({ file: cars });
+  }
+
+  getRandomPositionFromArray(list) {
+    const listLength = list.length;
+    return Math.floor(
+      Math.random() * (listLength)
+    );
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds);
+    const carId = carCategory.carIds[randomCarIndex];
+
+    return carId;
+  }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    return this.carRepository.find(carId);
+  }
+}
+
+module.exports = CarService;
