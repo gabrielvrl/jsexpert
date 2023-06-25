38b9c489c6c45ac74557fc868f251037447f1382
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-06-25 16:08:19 -0300

    Factory Design Pattern, N-Tier Architecture, Dependency Injection - Creational Patterns

---
+++ modulo06/creational/01.factory/test/userFactory.test.js
@@ -0,0 +1,32 @@
+const rewiremock = require('rewiremock/node');
+const { deepStrictEqual } = require('assert');
+
+// poderia estar em outro arquivo
+const dbData = [ { name: 'Gabriel' }, { name: 'Erick' } ]
+class MockDatabase {
+  connect = () => this;
+  find = async (query) => dbData;
+}
+// 
+
+rewiremock(() => require('../src/util/database')).with(MockDatabase);
+
+;(async () => {
+  {
+    const expected = [{ name: 'GABRIEL' }, { name: 'ERICK' }];
+    rewiremock.enable();
+    const UserFactory = require('../src/factory/userFactory');
+    const userFactory = await UserFactory.createInstance();
+    const result = await userFactory.find();
+    deepStrictEqual(result, expected);
+    rewiremock.disable();
+  }
+
+  {
+    const expected = [{ name: 'GABRIEL' }];
+    const UserFactory = require('../src/factory/userFactory');
+    const userFactory = await UserFactory.createInstance();
+    const result = await userFactory.find();
+    deepStrictEqual(result, expected);
+  }
+})();
